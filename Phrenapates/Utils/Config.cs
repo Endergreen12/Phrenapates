29466de021a0dfb46c11206d874c2708eafb2487
suna-aquatope <nanachi.yuzuru@gmail.com>
2024-10-09 21:30:58 +0700

    Update Config Version

--- Phrenapates/Utils/Config.cs
+++ Phrenapates/Utils/Config.cs
@@ -14,7 +14,7 @@ namespace Phrenapates.Utils
         public string IRCAddress { get; set; } = "127.0.0.1";
         public int IRCPort { get; set; } = 6667;
 
-        public string VersionId { get; set; } = "620f51baaf9942c1";
+        public string VersionId { get; set; } = "b9a286e529214957";
 
         public static void Load()
         {
