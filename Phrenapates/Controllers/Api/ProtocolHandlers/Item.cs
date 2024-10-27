39bb81b5fcdd68b3bf27a1ed102cac403e836fe2
ArkanDash <suckgamer0@Gmail.com>
2024-10-27 22:14:09 +0700

    Implement ItemAutoSynth

--- Phrenapates/Controllers/Api/ProtocolHandlers/Item.cs
+++ Phrenapates/Controllers/Api/ProtocolHandlers/Item.cs
@@ -33,19 +33,42 @@ namespace Phrenapates.Controllers.Api.ProtocolHandlers
         [ProtocolHandler(Protocol.Item_AutoSynth)]
         public ResponsePacket AutoSynthHandler(ItemAutoSynthRequest req)
         {
-            //TODO: Implement
-            /*var account = sessionKeyService.GetAccount(req.SessionKey);
+            var account = sessionKeyService.GetAccount(req.SessionKey);
             var parcelExcel = excelTableService.GetTable<ParcelAutoSynthExcelTable>().UnPack().DataList;
-            var itemExcel = excelTableService.GetTable<ItemExcelTable>().UnPack().DataList;
+            Dictionary<long, ItemDB> parcelResult = [];
 
             foreach (var parcel in req.TargetParcels)
             {
-                var parcelData = parcelExcel.FirstOrDefault(x => parcel.Equals(x.RequireParcelId));
-            }*/
+                var parcelData = parcelExcel.FirstOrDefault(x => x.RequireParcelId == parcel.Id);
+                var itemParcel = account.Items.FirstOrDefault(x => x.UniqueId == parcelData.RequireParcelId);
+
+                double totalItemRemoved = 0;
+                var totalSynthAdded = 0;
+                if(itemParcel.StackCount > parcelData.SynthStartAmount)
+                {
+                    totalItemRemoved = itemParcel.StackCount - parcelData.SynthEndAmount;
+                    totalSynthAdded = (int)Math.Floor(totalItemRemoved / parcelData.RequireParcelAmount);
+                }
+
+                var reducedItem = account.Items.FirstOrDefault(x => x.UniqueId == parcelData.RequireParcelId);
+                var synthItem = account.Items.FirstOrDefault(x => x.UniqueId == parcelData.ResultParcelId);
+                reducedItem.StackCount -= (int)totalItemRemoved;
+                synthItem.StackCount += totalSynthAdded;
+                parcelResult.Remove(reducedItem.UniqueId);
+                parcelResult.Remove(synthItem.UniqueId);
+                parcelResult.Add(reducedItem.UniqueId, reducedItem);
+                parcelResult.Add(synthItem.UniqueId, synthItem);
+                account.Items.FirstOrDefault(x => x.UniqueId == parcelData.RequireParcelId).StackCount = reducedItem.StackCount;
+                account.Items.FirstOrDefault(x => x.UniqueId == parcelData.ResultParcelId).StackCount = synthItem.StackCount;
+                context.SaveChanges();
+            }
 
             return new ItemAutoSynthResponse()
             {
                 ParcelResultDB = new()
+                {
+                    ItemDBs = parcelResult
+                }
             };
         }
     }
